1951912669bf4c5a347ee52b7b32c3ba4db54c32
VishaljiODEDRA <vishaljiodedra308@gmail.com>
2023-05-10 23:44:13 +0100

    Not only Vishwajeet, but Vishalji also

--- app.js
+++ app.js
@@ -4,15 +4,17 @@
         },'Vishwajeet from react')
 
         const heading2 = React.createElement('h2', {
-            id: 'heading2'
-        },
-        'This is heading 2')
+            id: 'heading'
+        }, 'This is heading 2')
 
         const container = React.createElement('div', {
             id: 'container'
         },
         [heading, heading2])
         console.log(heading)
+        console.log(heading2)
         const root = ReactDOM.createRoot(document.getElementById('root'))
+        const root2 = ReactDOM.createRoot(document.getElementById('root2'))
         //passing a react element inside the root. 
         root.render(container)
+        root2.render(container)
